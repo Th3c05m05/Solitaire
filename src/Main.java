bdbbe33eb93ea8c0e1621dedce2536f5d7af0450
Th3c05m05 <aekablan@gmail.com>
2020-04-17 09:58:06 -0600

    cards numbered 1-13 with change in name SK (spades king) -> 13S for simplicity of ranking

--- src/Main.java
+++ src/Main.java
@@ -8,64 +8,64 @@ public class Main {
     public static void makeDeck(){
 
         //Adds all the Spades
-        deck.add("SK");
-        deck.add("SQ");
-        deck.add("SJ");
-        deck.add("S10");
-        deck.add("S9");
-        deck.add("S8");
-        deck.add("S7");
-        deck.add("S6");
-        deck.add("S5");
-        deck.add("S4");
-        deck.add("S3");
-        deck.add("S2");
-        deck.add("S1");
+        deck.add("13S");
+        deck.add("12S");
+        deck.add("11S");
+        deck.add("10S");
+        deck.add("9S");
+        deck.add("8S");
+        deck.add("7S");
+        deck.add("6S");
+        deck.add("5S");
+        deck.add("4S");
+        deck.add("3S");
+        deck.add("2S");
+        deck.add("1S");
 
         //Adds all the Clubs
-        deck.add("CK");
-        deck.add("CQ");
-        deck.add("CJ");
-        deck.add("C10");
-        deck.add("C9");
-        deck.add("C8");
-        deck.add("C7");
-        deck.add("C6");
-        deck.add("C5");
-        deck.add("C4");
-        deck.add("C3");
-        deck.add("C2");
-        deck.add("C1");
+        deck.add("13C");
+        deck.add("12C");
+        deck.add("11C");
+        deck.add("10C");
+        deck.add("9C");
+        deck.add("8C");
+        deck.add("7C");
+        deck.add("6C");
+        deck.add("5C");
+        deck.add("4C");
+        deck.add("3C");
+        deck.add("2C");
+        deck.add("1C");
 
         //Adds all the Diamonds
-        deck.add("DK");
-        deck.add("DQ");
-        deck.add("DJ");
-        deck.add("D10");
-        deck.add("D9");
-        deck.add("D8");
-        deck.add("D7");
-        deck.add("D6");
-        deck.add("D5");
-        deck.add("D4");
-        deck.add("D3");
-        deck.add("D2");
-        deck.add("D1");
+        deck.add("13D");
+        deck.add("12D");
+        deck.add("11D");
+        deck.add("10D");
+        deck.add("9D");
+        deck.add("8D");
+        deck.add("7D");
+        deck.add("6D");
+        deck.add("5D");
+        deck.add("4D");
+        deck.add("3D");
+        deck.add("2D");
+        deck.add("1D");
 
         //Adds all the Hearts
-        deck.add("HK");
-        deck.add("HQ");
-        deck.add("HJ");
-        deck.add("H10");
-        deck.add("H9");
-        deck.add("H8");
-        deck.add("H7");
-        deck.add("H6");
-        deck.add("H5");
-        deck.add("H4");
-        deck.add("H3");
-        deck.add("H2");
-        deck.add("H1");
+        deck.add("13H");
+        deck.add("12H");
+        deck.add("11H");
+        deck.add("10H");
+        deck.add("9H");
+        deck.add("8H");
+        deck.add("7H");
+        deck.add("6H");
+        deck.add("5H");
+        deck.add("4H");
+        deck.add("3H");
+        deck.add("2H");
+        deck.add("1H");
 
         //Shuffles Deck
         Collections.shuffle(deck);
