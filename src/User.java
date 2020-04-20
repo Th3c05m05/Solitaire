789124a7d1c3b77b879459d2b3eb79ea52f493fb
Th3c05m05 <aekablan@gmail.com>
2020-04-19 18:37:13 -0600

    formatting fix

--- src/User.java
+++ src/User.java
@@ -4,6 +4,8 @@ public class User{
 
     static Scanner input = new Scanner(System.in);
 
+    
+
     public static void move(){
 
         System.out.println("it worked");
@@ -12,7 +14,9 @@ public class User{
 
 
     public static boolean workTest(String a, String b){
-        return Integer.parseInt(a.substring(0, 2)) == Integer.parseInt(b.substring(0, 2)) + 1 && ((a.substring(3, 4).equals("B") && b.substring(3, 4).equals("R")) || (a.substring(3, 4).equals("R") && b.substring(3, 4).equals("B")));
+        return Integer.parseInt(a.substring(0, 2)) == Integer.parseInt(b.substring(0, 2)) + 1
+                && ((a.substring(3, 4).equals("B") && b.substring(3, 4).equals("R"))
+                || (a.substring(3, 4).equals("R") && b.substring(3, 4).equals("B")));
     }
 
 }
