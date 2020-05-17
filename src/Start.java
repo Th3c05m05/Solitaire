9a9fcc715a5dd1787f200c8ad73c50efb11aff13
aaravsingh <saarav305@gmail.com>
2020-05-17 10:47:40 -0600

    New Update

--- src/Start.java
+++ src/Start.java
@@ -1,18 +1,16 @@
-import java.util.*;
-import javax.swing.*;
-import java.awt.*;
-import java.io.*;
-import java.awt.event.*;
-import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 
 public class Start {
     public static void main(String args[]){
-        //SOS (Solitaire on SteriodsMu)
+        //SOS (Solitaire on Steriods)
 
         String musicLocation = "/Desktop/Music/Scary.wav";
 
@@ -49,8 +47,42 @@ public class Start {
      JPanel panel = new JPanel();
      JButton button = new JButton("Click for Computer against AI");
      JButton instruct = new JButton(" Click for Instructions");
+     JLabel textAdder = new JLabel();
 
-        instruct.addActionListener(e->{
+
+     JTextField textField = new JTextField("Enter your name here!", 30);//the input
+     textField.setSize(7,7);
+     textField.setAlignmentX(0);
+     textField.setAlignmentY(0);
+
+
+
+     textField.addActionListener(new ActionListener() {
+                                     @Override
+                                     public void actionPerformed(ActionEvent e) {
+                                         String input = textField.getText();
+                                         textAdder.setText(input);
+
+                                     }
+                                 }
+
+
+     );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            instruct.addActionListener(e->{
 
 
 
@@ -63,12 +95,12 @@ public class Start {
                      "   Seven piles...First rotation lays card to the very left up and the rest of the six down.\n" +
                      "   Next place a card upright on the second pile and then the rest down\n" +
                      "   Continue this until every pile has one face up card on the top\n" +
-                     "   */");
+                     "   ");
 
             JLabel object = new JLabel("/*OBJECT:\n" +
                     "   The object of the game is to place cards on the ACE of a different suit\n" +
                     "   THE GAME is won by transferring all remaining cards to the ACE pile\n" +
-                    "   */");
+                    "   ");
 
             JLabel gameplay = new JLabel("/*GAMEPLAY\n" +
                     "   Cards can be transferred to other piles in the foundation if the card being transferred is one lower AND has a different color than the card that is placed on\n" +
@@ -79,7 +111,7 @@ public class Start {
             JLabel rules = new JLabel("/*RULES\n" +
                     "   When one of the piles is gone, only a king can be moved to that open space\n" +
                     "   All piles must have one FACE UP card\n" +
-                    "   */");
+                    "   ");
 
 
 
@@ -89,9 +121,13 @@ public class Start {
             panel2.add(object);
             panel2.add(gameplay);
             panel2.add(rules);
+            panel2.setBackground(Color.PINK);
             instructor.add(panel2);
             instructor.setSize(500,500);
+
+            instructor.pack();
             instructor.setVisible(true);
+
 
 
 
@@ -102,16 +138,26 @@ public class Start {
 
 
 
-
-        panel.add(button);
-     panel.add(label);
-     panel.add(instruct);
-     panel.setBackground(new Color(148,250,255,50));
-
+        //frame.setBackground(new Color(148,250,255,50));
+        frame.setVisible(true);
+        frame.setLocation(500,-500);
+        //frame.pack();
+        frame.setSize(400,400);
+            panel.setBackground(Color.PINK);
         frame.add(panel);
-     frame.setVisible(true);
 
-     frame.setSize(500,700);
+     panel.add(label);
+        panel.add(button);
+     panel.add(instruct);
+        panel.add(textField);
+        panel.add(textAdder);
+
+
+
+
+
+
+
 
     }
 
