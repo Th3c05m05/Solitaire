1f1246284953f6ab64cb9c944fb412560ebf1331
aaravsingh <saarav305@gmail.com>
2020-06-03 00:59:29 -0400

    New Update

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,7 @@ public class Main extends Start {
         piler.add(deck.get(0));//this indicates the beginning of the game. Once card of the deck is turned over and placed into the pile
         deck.remove(deck.get(0));
 
-        int x=0;
-
+        int x = 0;
 
 
         Main obj = new Main();
@@ -25,7 +24,7 @@ public class Main extends Start {
             System.out.println("This is your hand" + hand);
             System.out.println("Select an index number. If you are out of option press type -1. If you want to quit the game press -5");
             int index = input.nextInt();
-            if (index > hand.size()) {
+            if (index > hand.size() - 1) {
                 System.out.println("Please try again");
             } else if (index == -1) {
                 hand.add(deck.get(0));
@@ -33,30 +32,39 @@ public class Main extends Start {
                 for (int r = 0; r < AI.size(); r++) {
                     if ((AI.get(r).substring(3, 4).equals(piler.get(piler.size() - 1).substring(3, 4))) && (AI.get(r).substring(2, 3).equals(piler.get(piler.size() - 1).substring(2, 3)))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
-                    } else if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
+                    }
+                    if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
                     }
 
 
                 }
-                if(reductionAI.size()==0){
+                if (reductionAI.size() == 0) {
                     AI.add(deck.get(0));
                     deck.remove(deck.get(0));
-                    System.out.println("This is the AI set:"+AI);
-                }
-                else if(reductionAI.size()>1){
-                    randomChose = objGenerator.nextInt(reductionAI.size() - 1);
-
-                    AI.remove(reductionAI.get(randomChose));
-                    piler.add(reductionAI.get(randomChose));
-                    //System.out.println(reductionAI.get(randomChose));
                     System.out.println("This is the AI set:" + AI);
+                } else if (reductionAI.size() >= 1) {
+                    if (reductionAI.size() == 1) {
+                        randomChose = 0;
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set: " + AI);
+                        reductionAI.clear();
+                    } else {
+                        randomChose = objGenerator.nextInt(reductionAI.size() - 1);
+
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        //System.out.println(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set:" + AI);
+                        reductionAI.clear();
+                    }
                 }
                 if (AI.size() == 1) {
                     System.out.println("UNO FOR AI");
@@ -79,30 +87,40 @@ public class Main extends Start {
                 for (int r = 0; r < AI.size(); r++) {
                     if ((AI.get(r).substring(3, 4).equals(piler.get(piler.size() - 1).substring(3, 4))) && (AI.get(r).substring(2, 3).equals(piler.get(piler.size() - 1).substring(2, 3)))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
-                    } else if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
+                    }
+                    if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
                     }
 
 
                 }
-                if(reductionAI.size()==0){
+                if (reductionAI.size() == 0) {
                     AI.add(deck.get(0));
                     deck.remove(deck.get(0));
-                    System.out.println("This is the AI set:"+AI);
-                }
-                else if(reductionAI.size()>1){
-                    randomChose = objGenerator.nextInt(reductionAI.size() - 1);
-
-                    AI.remove(reductionAI.get(randomChose));
-                    piler.add(reductionAI.get(randomChose));
-                    //System.out.println(reductionAI.get(randomChose));
                     System.out.println("This is the AI set:" + AI);
+                    reductionAI.clear();
+                } else if (reductionAI.size() >= 1) {
+                    if (reductionAI.size() == 1) {
+                        randomChose = 0;
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set: " + AI);
+                        reductionAI.clear();
+                    } else {
+                        randomChose = objGenerator.nextInt(reductionAI.size() - 1);
+
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        //System.out.println(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set:" + AI);
+                        reductionAI.clear();
+                    }
                 }
                 if (AI.size() == 1) {
                     System.out.println("UNO FOR AI");
@@ -122,30 +140,42 @@ public class Main extends Start {
                 for (int r = 0; r < AI.size(); r++) {
                     if ((AI.get(r).substring(3, 4).equals(piler.get(piler.size() - 1).substring(3, 4))) && (AI.get(r).substring(2, 3).equals(piler.get(piler.size() - 1).substring(2, 3)))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
-                    } else if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
+                    }
+                    if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
                     }
 
 
                 }
-                if(reductionAI.size()==0){
+                if (reductionAI.size() == 0) {
                     AI.add(deck.get(0));
                     deck.remove(deck.get(0));
-                    System.out.println("This is the AI set:"+AI);
-                }
-                else if(reductionAI.size()>=1){
-                    randomChose = objGenerator.nextInt(reductionAI.size() - 1);
-
-                    AI.remove(reductionAI.get(randomChose));
-                    piler.add(reductionAI.get(randomChose));
-                    //System.out.println(reductionAI.get(randomChose));
                     System.out.println("This is the AI set:" + AI);
+                }
+
+                if (reductionAI.size() >= 1) {
+                    if (reductionAI.size() == 1) {
+                        randomChose = 0;
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set: " + AI);
+                        reductionAI.clear();
+                    } else {
+                        randomChose = objGenerator.nextInt(reductionAI.size() - 1);
+
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        //System.out.println(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set:" + AI);
+                        reductionAI.clear();
+
+                    }
                 }
                 if (AI.size() == 1) {
                     System.out.println("UNO FOR AI");
@@ -163,15 +193,15 @@ public class Main extends Start {
                 //hand.add(deck.get(0));
                 //deck.remove(deck.get(0));
                 //obj.checkAI();
-                for (int r = 0; r < AI.size(); r++) {
+                /*for (int r = 0; r < AI.size(); r++) {
                     if ((AI.get(r).substring(3, 4).equals(piler.get(piler.size() - 1).substring(3, 4))) && (AI.get(r).substring(2, 3).equals(piler.get(piler.size() - 1).substring(2, 3)))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
-                    } else if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
+                    } if (Integer.parseInt(AI.get(r).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
 
-                        reductionAI.add(AI.get(x));
+                        reductionAI.add(AI.get(r));
 
 
                     }
@@ -183,13 +213,21 @@ public class Main extends Start {
                     deck.remove(deck.get(0));
                     System.out.println("This is the AI set:"+AI);
                 }
-                else if(reductionAI.size()>1){
-                    randomChose = objGenerator.nextInt(reductionAI.size() - 1);
 
-                    AI.remove(reductionAI.get(randomChose));
-                    piler.add(reductionAI.get(randomChose));
-                    //System.out.println(reductionAI.get(randomChose));
-                    System.out.println("This is the AI set:" + AI);
+                if(reductionAI.size()>=1){
+                    if(reductionAI.size()==1){
+                        randomChose=0;
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set: "+AI);
+                    }else {
+                        randomChose = objGenerator.nextInt(reductionAI.size() - 1);
+
+                        AI.remove(reductionAI.get(randomChose));
+                        piler.add(reductionAI.get(randomChose));
+                        //System.out.println(reductionAI.get(randomChose));
+                        System.out.println("This is the AI set:" + AI);
+                    }
                 }
                 if (AI.size() == 1) {
                     System.out.println("UNO FOR AI");
@@ -208,51 +246,14 @@ public class Main extends Start {
             if (hand.size() == 0) {
                 System.out.println("Congratulations, you have won against the AI");
                 System.exit(0);
+            }*/
+
+
+                // }
             }
 
 
         }
-    }
-
-
-    public void checkAI() {//checkAI has been having some minor issues...will try to see tm
-
-
-
-        /*for (int x = 0; x < AI.size(); x++) {
-            if ((AI.get(x).substring(3, 4).equals(piler.get(piler.size() - 1).substring(3, 4))) && (AI.get(x).substring(2, 3).equals(piler.get(piler.size()-1).substring(2, 3)))) {
-
-                reductionAI.add(AI.get(x));
-
-
-            } else if (Integer.parseInt(AI.get(x).substring(0, 2)) == Integer.parseInt(piler.get(piler.size() - 1).substring(0, 2))) {
-
-                reductionAI.add(AI.get(x));
-
-
-            } else {
-                AI.add(deck.get(0));
-                deck.remove(deck.get(0));
-            }
-
-
-        }
-
-        randomChose = objGenerator.nextInt(reductionAI.size() - 1);
-
-        AI.remove(reductionAI.get(randomChose));
-        piler.add(reductionAI.get(randomChose));
-        //System.out.println(reductionAI.get(randomChose));
-        System.out.println("This is the AI set:" + AI);
-        if(AI.size()==1){
-            System.out.println("UNO FOR AI");
-        }
-        if(AI.size()==0) {
-            System.out.println("THE AI HAS WON");
-            System.exit(0);
-        }
-
-    }*/
     }
 }
 
